532b3e46d62b5defe73095a6b0d44eb300698693
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-09-23 03:40:40 +0000

    Epi-4 Learn Basics With Investigation

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,25 @@
-console.log("Keshav");
+const accountId =  14453
+let acccountEmail = "Madhav@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2  Not allowed
+
+
+let accountEmail = "Keshav@golok.com"
+accountPassword = "54321"
+accountCity = "Braj"
+
+let accountState;
+
+
+
+
+console.log(accountCity);
+
+/*
+Prefer not to use var
+because of issue in block scope and function
+*/
+
+console.table([accountId, acccountEmail, accountPassword, accountCity, accountState]);
