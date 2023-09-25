907c22270c7aaeee1b051990cd00416b43d6012a
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-09-25 11:27:22 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) : We are using node JS not browser
+
+console.log(3
+    +3); //Not a Good Practice..Code readability should be high
+
+    let name = "Keshav"
+    let age = 5200
+    let isLoggedIn = false
+    let state;
+
+    //number => 2 to power 53
+    //bigint
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined =>
+    // symbol => unique
+
+
+    //object
+
+    console.log(typeof undefined); // undefined      
+    console.log(typeof null); // object
+
+    
