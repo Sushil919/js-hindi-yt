f728d12d8fbdbf7a49952bd95f44b05acd849f7a
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-09-26 10:38:51 +0000

    Printing -ve Value and Concatenating two strings

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,35 +1,47 @@
-let score = "33abc"
-console.log(typeof score);
-console.log(typeof (score));
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
-
-// "33" => 33
-// "33abc" => NaN
-//true => 1; false =>0
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-let isLoggedIn = 1
+// // 33 => number
+// // "33" => stirng
+// // "33abc" => NaN
+// // null => 0
+// //undefined => Nan 
+// //true => 1; false =>0
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-let isLoggedIn = "Keshav"
+// let isLoggedIn = "Keshav"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
-// "" => false
-// "hitesh" => true 
+// // 1 => true; 0 => false
+// // "" => false
+// //"hitesh" => true 
 
-let some
+/************************* OPERATIONS*********************************************** */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Radhe Radhe"
+let str2 = " Kanha"
+let str3 = str1+str2
+console.log(str3);
