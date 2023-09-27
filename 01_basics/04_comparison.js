d6a213a2456fbf4d4e4cf3676794189b41d8ebe8
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-09-27 14:17:21 +0000

    Avoid some type of comparisons

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,4 +8,16 @@ console.log(null >= 0);  //true
 // ............................................................................................................................
 
 /* Note : The reason is that an equality check == and compariions > , < , >= , <= work differently.
- Comparisons convert null to a number*/
+ Comparisons convert null to a number, treating it as 0
+ That's why (3) null >= 0 is true and (1) null > 0 is false.*/
+
+ console.log(undefined == 0); 
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+
+ //Note : Try to avoid above type of conversion coz ye kabhi bhi confusion me daal sakte hain
+
+ //Clean Code ki value hai usi ko use karte hain
+
+ // //======================//
+ console.log("2" === 2);
