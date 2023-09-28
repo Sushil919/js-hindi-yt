fd7abf866db052e914695820b0ca999bbe82c862
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-09-28 12:21:51 +0000

    datatype summary

---
+++ datatypes_summary.js
@@ -0,0 +1,30 @@
+//Datatypes Summary
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 2345546775455433452167n
+
+//Reference (Non Primitive)
+
+const heros = ["Eklavya", "Karn", "Arjuna"];
+
+let myObj = {
+    name: "Keshav",
+    age: 22
+}
+
+//Can Treat a function as a variable in JS
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+//*****For return types=>**************https://262.ecma-international.org/5.1/#sec-11.4.3 */
