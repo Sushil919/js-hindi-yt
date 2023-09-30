889bf82141b4e8693940202314206d7b9041a363
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-09-30 12:12:49 +0000

    USing ` ` beside + operator

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,3 +1,7 @@
 const name = "Keshav"
  const repoCount = 50
- console.log(name + repoCount + " Value");
+//  console.log(name + repoCount + " Value");  // Not recomended using + to conccate
+
+//&& Now a days string interpolation is used `  ` :
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
