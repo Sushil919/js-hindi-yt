d00cdfa6f57154d15ae24421854c7f47c96396f7
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-03 08:31:53 +0000

    charAt and indexOf

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,12 +1,16 @@
-const name = "Keshav"
- const repoCount = 50
-//  console.log(name + repoCount + " Value");  // Not recomended using + to conccate
+// const name = "Keshav"
+//  const repoCount = 50
+// //  console.log(name + repoCount + " Value");  // Not recomended using + to conccate
 
-//&& Now a days string interpolation is used `  ` :
+// //&& Now a days string interpolation is used `  ` :
 
-console.log(`My name is ${name} and my repo count is ${repoCount}`)
+// console.log(`My name is ${name} and my repo count is ${repoCount}`)
 
 const gameName = new String ('hiteshssc');
 
-console.log(gameName [0]); //Accessing the 0th pos element
-console.log(gameName [1]); //Accessing the 1st pos element
+// console.log(gameName [0]); //Accessing the 0th pos element
+// console.log(gameName.__proto__); // Syntax for Accessing the prototye method
+
+//*****Accessing the char pos */
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
