cc25ea02b84b1def134acc04147feb4b4af02f61
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-07 23:26:39 +0000

    Precised value

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -1,10 +1,15 @@
-const score = 400 // Automatically detects the data type
-console.log(score);
+// const score = 400 // Automatically detects the data type
+// console.log(score);
 
-const balance = new Number(100) //defined new object of type number
-console.log(balance);
+// const balance = new Number(100) //defined new object of type number
+// console.log(balance);
 
-//Lets make it string
+// //Lets make it string
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+//When we need to print precised value while writing code for ecom app
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3));
