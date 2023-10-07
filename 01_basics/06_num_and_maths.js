fc9c83a20d63480b1939da759a2ebf8cd67426a9
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-07 23:24:11 +0000

    toFixed()

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -3,3 +3,8 @@ console.log(score);
 
 const balance = new Number(100) //defined new object of type number
 console.log(balance);
+
+//Lets make it string
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
