e55726d1efaf82afaae1566468836565c1451d72
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-10 08:27:20 +0000

    const hundreds

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -13,3 +13,8 @@
 //When we need to print precised value while writing code for ecom app
 const otherNumber = 123.8966
 console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); //output acc US Std.
+//Specify to Indian num system
+console.log(hundreds.toLocaleString('en-IN'));
