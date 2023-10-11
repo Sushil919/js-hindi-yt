e1d659e91cd50c1ecf3a30002c0ad708497bb84c
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-11 06:11:30 +0000

    check typeof

--- 01_basics/07_dateinjs.js
+++ 01_basics/07_dateinjs.js
@@ -4,7 +4,13 @@ let myDate = new Date()
 // console.log(myDate); // not readable
 
 // //lets convert it into string
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.toJSON());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+//lets check the type of date
+
+console.log(typeof myDate);
