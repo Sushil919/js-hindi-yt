34d709554fa79d40373eea4999adc4c5fd9832bd
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-13 16:25:26 +0000

    array val

---
+++ 02_basics/02_array.js
@@ -0,0 +1,5 @@
+const dwapar_heros = ["madhav", "karn", "arjun", "bheem"]
+const treta_heros = ["shriram", "bharat", "bajrangi"]
+
+dwapar_heros.push(treta_heros)
+console.log(dwapar_heros); //array koi bhi value le leta hai ..pure array ko hi as one element
