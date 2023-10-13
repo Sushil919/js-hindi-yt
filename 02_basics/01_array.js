8c69b2880cd4b9924de40b542d5042ca6449c22b
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-13 15:32:08 +0000

    includes

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -18,7 +18,9 @@ const myHeros = ["Keshav", "Parth"]
 // myArr.pop();
 // console.log(myArr);
 
-myArr.unshift(9); //add at leftmost place by shifting all the values right
-console.log(myArr);
-myArr.shift() //unlike pop it removes the left most
-console.log(myArr);
+// myArr.unshift(9); //add at leftmost place by shifting all the values right
+// console.log(myArr);
+// myArr.shift() //unlike pop it removes the left most
+// console.log(myArr);
+
+console.log(myArr.includes(9)); //to know 9 is included or not
