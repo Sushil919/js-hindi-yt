d121eb53bb66c9e451ab41a70127734eec97b446
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-13 16:53:20 +0000

    concat array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,5 +1,12 @@
 const dwapar_heros = ["madhav", "karn", "arjun", "bheem"]
 const treta_heros = ["shriram", "bharat", "bajrangi"]
 
-dwapar_heros.push(treta_heros)
-console.log(dwapar_heros); //array koi bhi value le leta hai ..pure array ko hi as one element
+// dwapar_heros.push(treta_heros)
+// console.log(dwapar_heros); //array koi bhi value le leta hai ..pure array ko hi as one element
+
+//access elements
+// console.log(dwapar_heros);
+// console.log(dwapar_heros[3][1]);
+
+const allHeros = dwapar_heros.concat(treta_heros)
+console.log(allHeros);
