4c93786ee28ac493971769b735e36bf1bbde93c5
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-14 16:26:29 +0000

    from of isArray

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -19,4 +19,17 @@ const treta_heros = ["shriram", "bharat", "bajrangi"]
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// console.log(real_another_array);
+
+//Pooch bhi sakte hain
+
+console.log(Array.isArray("Keshav"));
+console.log(Array.from("Keshav"));
+
+console.log(Array.from({name: "hitesh"})); //interesting
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
