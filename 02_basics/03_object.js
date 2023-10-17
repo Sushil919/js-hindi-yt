c1f54583c2277bcf57dc8239856e5c813fac31e6
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-17 10:20:44 +0000

    usual way to access the object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -2,8 +2,18 @@
 //Literal ki tarah declare karte hain to singleton nhi banta...constructor se humesha singleton banega
 // const JsUser = {} //empty object
 
+// const JsUser = {
+//     name:"Keshav"
+// } //here we can define both keys and values unlike array
+
+//object : Declaration
+
 const JsUser = {
-    name:"Keshav"
-} //here we can define both keys and values unlike array
-
-
+    full_name : "Shri Nivas",
+    age : 19,
+    location : "Venkatesh",
+    email : "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+console.log(JsUser.email); //access the object
