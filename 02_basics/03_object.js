84d9bd0f0e87d12d5f11f01af9cdd92a3d524c31
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-17 10:08:56 +0000

    object difinition

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,3 +1,9 @@
 //object literals
 //Literal ki tarah declare karte hain to singleton nhi banta...constructor se humesha singleton banega
-const JsUser = {} //empty object
+// const JsUser = {} //empty object
+
+const JsUser = {
+    name:"Keshav"
+} //here we can define both keys and values unlike array
+
+
