c01edf493913fbc43355885660d6c7d0272e13f8
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-22 21:52:53 +0000

    empty object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,2 +1,8 @@
 //Object part 2
 //Singleton : how can we declare with help of constructor
+
+// const tinderUser = new Object() //singleton
+// console.log(tinderUser); //empty object
+
+const tinderUser = {} //non singleton
+console.log(tinderUser); //empty object
