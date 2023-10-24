35ca641b37c1305d1935daf4fb99a2812a01e732
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-24 23:06:21 +0000

    destructuring the object

---
+++ 02_basics/05_object.js
@@ -0,0 +1,7 @@
+ //object ka discussion hi future me banega API ka discussion
+
+ //Destructure of Objects
+ //.....React padhenge to object milega...U object ko destructure karke uski values leni padegi
+ //\\Direct v le sakte hain values ko bu detructuring is bit easy
+
+ 
