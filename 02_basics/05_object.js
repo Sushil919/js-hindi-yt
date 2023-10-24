3c0a64ad2986ea09b7e0f2c97ab693b93ae09305
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-24 23:13:02 +0000

    object create and access

--- 02_basics/05_object.js
+++ 02_basics/05_object.js
@@ -4,4 +4,9 @@
  //.....React padhenge to object milega...U object ko destructure karke uski values leni padegi
  //\\Direct v le sakte hain values ko bu detructuring is bit easy
 
- 
+ const course = {
+    courseName : "JS in hindi",
+    courePrice : "999",
+    courseInstructure : "Hitesh Sir"
+ }
+course.courseInstructure //access kar sakte hain bilkul shi hai ya Square notation v padh sakte hain
