6d13edaf2966b1994683b52d8ce9928ef0075987
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-27 11:44:56 +0000

    yhi problem aati hai

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,5 +11,6 @@ sayMyname //kuch nhi aaya...coz its just reference....() laga ke execution hota 
 function addTwoNumbers(number1, number2){
     console.log(number1+number2);
 }
-addTwoNumbers(3,4)
-addTwoNumbers(3,"4") //srting soch ke concat kar diya
+//addTwoNumbers(3,4)
+//addTwoNumbers(3,"4") //string soch ke concat kar diya
+addTwoNumbers(3,"a") //string soch ke concat kar diya
