1abdbf8e75a430235215445851dcdf4583b7b29b
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-27 11:52:51 +0000

    parameters and arguements

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,11 +8,11 @@ function sayMyname(){
 }
 sayMyname //kuch nhi aaya...coz its just reference....() laga ke execution hota hai
 
-function addTwoNumbers(number1, number2){
+function addTwoNumbers(number1, number2){ //Parameters{When defining}
     console.log(number1+number2);
 }
 //addTwoNumbers(3,4)
 //addTwoNumbers(3,"4") //string soch ke concat kar diya
 // addTwoNumbers(3,"a") //string soch ke concat kar diya
 
-addTwoNumbers(3,null)
+//addTwoNumbers(3,null)//Arguements {when calling}
