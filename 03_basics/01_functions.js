b2af86dc10ebeb77a263837ec71c830ee07cd496
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-28 17:05:05 +0000

    now result got executed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,21 +1,26 @@
-function sayMyname(){
-    console.log("K");
-    console.log("E");
-    console.log("S");
-    console.log("H");
-    console.log("A");
-    console.log("V");
-}
-sayMyname //kuch nhi aaya...coz its just reference....() laga ke execution hota hai
+// function sayMyname(){
+//     console.log("K");
+//     console.log("E");
+//     console.log("S");
+//     console.log("H");
+//     console.log("A");
+//     console.log("V");
+// }
+//sayMyname //kuch nhi aaya...coz its just reference....() laga ke execution hota hai
 
-function addTwoNumbers(number1, number2){ //Parameters{When defining}
-    console.log(number1+number2); //console doesnot mean ki wo fnction value return v kar rha 
-}
+//function addTwoNumbers(number1, number2){ //Parameters{When defining}
+   // console.log(number1+number2); //console doesnot mean ki wo fnction value return v kar rha 
+//}
 //addTwoNumbers(3,4)
 //addTwoNumbers(3,"4") //string soch ke concat kar diya
 // addTwoNumbers(3,"a") //string soch ke concat kar diya
 
 //addTwoNumbers(3,null)//Arguements {when calling}
 
+function addTwoNumbers(number1, number2){
+    let result = number1+number2
+    return result
+}
+
 const result = addTwoNumbers(3,4)
-console.log("Result : ",result); //result me to undefined hai
+console.log("Result : ",result) //result me to undefined hai
