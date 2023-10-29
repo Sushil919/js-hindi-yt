f64c70219000a3f79369f09fcee3d455eba83a4f
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-29 17:16:02 +0000

    val1, val2, ...num1

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -1,5 +1,11 @@
-function calculateCartPrice(...num1) //...rest operator...sometimes spread operator**depends on situation
+// function calculateCartPrice(...num1) //...rest operator...sometimes spread operator**depends on situation
+// {
+//     return num1
+// }
+// console.log(calculateCartPrice(200, 400, 500)); //ab array ke andar add karke mil rha
+
+function calculateCartPrice(val1, val2,...num1) 
 {
     return num1
 }
-console.log(calculateCartPrice(200, 400, 500)); //ab array ke andar add karke mil rha
+console.log(calculateCartPrice(200, 400, 500, 2000)); //starting ki 2 val val1, val2 and rest num1 me chali jayengi
