1aa11a65467a7c3520e7e105c65af83fb79091bd
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-29 17:13:18 +0000

    rest operator...

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -1,4 +1,5 @@
-function calculateCartPrice(num1){
+function calculateCartPrice(...num1) //...rest operator...sometimes spread operator**depends on situation
+{
     return num1
 }
-console.log(calculateCartPrice(2));
+console.log(calculateCartPrice(200, 400, 500)); //ab array ke andar add karke mil rha
