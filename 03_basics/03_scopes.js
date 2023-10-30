c60499cd3dca0fe2ab891f54657705c6a909f863
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-30 16:51:55 +0000

    scope

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -17,5 +17,17 @@ function one(){
     // console.log(website); //notice : line by line exec hota hai ..ye error de diya..so iske next two() exec hi nhi hua
     two()
 }
-one()
+//one()
 //**Error se hi seekha jata hai..jitna error ke sath familiar honge utne achhe se development kar payenge */
+
+// %%Closure kya hai ye achhe se..DOM (Kaise HTML ki page ko manupulate kiya jata hai javascript ke andar) padhne pe samjh aayega 
+
+if(true){
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    console.log(website);
+}
+console.log(username);
