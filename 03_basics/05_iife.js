5c430e1e1ffad58143cbaf3986ab8281bbb3eded
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-31 18:28:22 +0000

    iife

--- 03_basics/05_iife.js
+++ 03_basics/05_iife.js
@@ -1,5 +1,9 @@
 //Immediately Invoked Function Expression (IIFE) 
 //Global Scope ke pollution se problem hoti h kai baar
 
+(function chai(){
+    console.log(`DB Connected`);
+}) ()
+
 //TO Global Scope me jitne variable ahain ..jo bhi pollution hai,,
 //usko hatane ke liye, we use IIFE
