48ecbaa13c68ed8b05c17ae8a019a88ccedde0a8
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-31 17:05:36 +0000

    current context print

--- 03_basics/04_arrowfn.js
+++ 03_basics/04_arrowfn.js
@@ -6,6 +6,7 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`); //current context ko refer karne ke liye "this" keyword lagate hain
+        console.log(this); //this jo v current values hain unki baat karta hai..current context ki baat karta hai
     }
 }
 user.welcomeMessage() //value is not hardquoted,,,,,jo bhi current context hai us current context me username ke andar jo bhi value hai...usko print karo
