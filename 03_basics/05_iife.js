498e6b1d1404bc29e26d115b57bd2a4392d6b01c
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-10-31 18:24:42 +0000

    IIfe used

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,5 @@
+//Immediately Invoked Function Expression (IIFE) 
+//Global Scope ke pollution se problem hoti h kai baar
+
+//TO Global Scope me jitne variable ahain ..jo bhi pollution hai,,
+//usko hatane ke liye, we use IIFE
