4cb22bde1dc72943e2f3cfb02bc39d9967b8978a
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-01 23:13:44 +0000

    if scope in single line

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -1,9 +1,13 @@
-const score = 200
+// const score = 200
 
-if (score >  100){
+// if (score >  100){
 
-const power = "fly"  //scope of fly is local
-console.log(`user power : ${power}`);
+// const power = "fly"  //scope of fly is local
+// // console.log(`user power : ${power}`);
 
-}
-console.log(`user power : ${power}`); //Error , cant access fly bcoz its in the scope of if
+//}
+// console.log(`user power : ${power}`); //Error , cant access fly bcoz its in the scope of if
+
+const balance = 1000
+
+if(balance > 500) console.log("test"), console.log("test2"); //bad practice
