9ed6cea47eba8231995525fde99e1649f072f5e1
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-02 21:30:28 +0000

    truthy value

--- 04_controlFlow/02.js
+++ 04_controlFlow/02.js
@@ -1,26 +1,37 @@
-const month = "March"
-switch(month){
-    case "Jan" :
-        console.log("Jan");
-        break;
+// const month = "March"
+// switch(month){
+//     case "Jan" :
+//         console.log("Jan");
+//         break;
 
-    case "Feb" :
-        console.log("Feb");
-        break;
+//     case "Feb" :
+//         console.log("Feb");
+//         break;
 
-    case "March" :
-        console.log("March");
-        break;               //break nhi hai to : condt ke baad ke saare cases execute ho jate hain
-                                //except default case
+//     case "March" :
+//         console.log("March");
+//         break;               //break nhi hai to : condt ke baad ke saare cases execute ho jate hain
+//                                 //except default case
 
-    case 4 :
-        console.log("Apr");
-        break;
+//     case 4 :
+//         console.log("Apr");
+//         break;
 
-    case 5 :
-        console.log("May");
-        break;
+//     case 5 :
+//         console.log("May");
+//         break;
 
-    default :
-        console.log("Default case run");
+//     default :
+//         console.log("Default case run");
+// }
+
+
+const userEmail = "parth@google.com"
+
+if(userEmail) //kuch condt check nhi kiya
+                //String is considered as a truthy val
+{
+    console.log("Email id authenticated"); //email hone ki wajah se its getting executed
 }
+else
+console.log("Id not authenticated");
