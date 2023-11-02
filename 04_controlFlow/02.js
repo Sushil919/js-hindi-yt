191ebccbe5d84153e86810ffe67583ef81eb8db9
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-02 21:11:31 +0000

    switch case

---
+++ 04_controlFlow/02.js
@@ -0,0 +1,25 @@
+const month = 3
+switch(month){
+    case 1 :
+        console.log("Jan");
+        break;
+
+    case 2 :
+        console.log("Feb");
+        break;
+
+    case 3 :
+        console.log("March");
+        break;
+
+    case 4 :
+        console.log("Apr");
+        break;
+
+    case 5 :
+        console.log("May");
+        break;
+
+    default :
+        console.log("Default case run");
+}
