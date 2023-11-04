961690684a608acf926e0d1482b9917e67fdeeda
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-04 12:01:20 +0000

    for loop array

--- 05_iterators/one.js
+++ 05_iterators/one.js
@@ -1,8 +1,16 @@
-for (let i = 1; i<=10 ; i++){
-    console.log(" ");
-    console.log(`Outer loop value : ${i} \n The table of ${i} is`);
-    for (let j = 1; j<=10; j++){
-        console.log(" ");
-        console.log(`${i} * ${j} = ${i*j}`);
+// for (let i = 1; i<=10 ; i++){
+//     console.log(" ");
+//     console.log(`Outer loop value : ${i} \n The table of ${i} is`);
+//     for (let j = 1; j<=10; j++){
+//         console.log(" ");
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+    let myArray = ["Keshav", "Parth", "Karn"]
+    console.log(myArray.length);
+    for (let index = 0; index < myArray.length; index++) {
+        const element = myArray[index];
+        console.log(element);
+        
     }
-}
