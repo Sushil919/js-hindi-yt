58840b13ce809056c651507f935839de35b16599
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-05 22:33:11 +0000

    while

--- 05_iterators/two.js
+++ 05_iterators/two.js
@@ -3,3 +3,11 @@ while(index<=10){
     console.log(`Value of index is ${index}`);
     index+=2
 }
+
+let myArray = ["Raghav", "Bharat", "Lakshman", "Shatrughn"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr =  arr + 1
+}
