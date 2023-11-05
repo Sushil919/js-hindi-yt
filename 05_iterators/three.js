b6bda1e0851881f2320be06a55311e1f1fd04c5b
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-05 23:09:32 +0000

    higher order array loops

---
+++ 05_iterators/three.js
@@ -0,0 +1,9 @@
+//Higher order Array loops
+//Array specific loops
+
+//for of 
+
+// ["","",""]
+// [{},{},{}] //Ye boht common scenario hai ki array ke andar aapko boht saare objects milenge
+                //aur har iteration me aapko objects me se koi value nikalni hai
+//Pehle loop lagaiye fir object ke andar jayiye ....fir . notation ya [] notation se ek value le aayiye
