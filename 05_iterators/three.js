f344e5b2024b02a0df6b0a369f7472980633292a
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-05 23:29:39 +0000

    iterator of object

--- 05_iterators/three.js
+++ 05_iterators/three.js
@@ -7,3 +7,13 @@
 // [{},{},{}] //Ye boht common scenario hai ki array ke andar aapko boht saare objects milenge
                 //aur har iteration me aapko objects me se koi value nikalni hai
 //Pehle loop lagaiye fir object ke andar jayiye ....fir . notation ya [] notation se ek value le aayiye
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const iterator of object) //ab ye jo iteraror of object hai isse don worry ...aisa nhi h ki ye 
+                                // loop object ke liye hi kaam aata hai....loop, loop hai --> kisi ka v iterator ho skta hai..
+//Objects, Array, String sare iteratable hain..unpe loops laga sakte hain
+
+{
+    
+}
