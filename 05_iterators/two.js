0e3661c7a21c88ed9022bf71298d2c99069d0fb6
Sushil Kumar <79791640+Sushil919@users.noreply.github.com>
2023-11-05 23:00:56 +0000

    do while

--- 05_iterators/two.js
+++ 05_iterators/two.js
@@ -1,13 +1,8 @@
-let index = 1
-while(index<=10){
-    console.log(`Value of index is ${index}`);
-    index+=2
-}
+// 
 
-let myArray = ["Raghav", "Bharat", "Lakshman", "Shatrughn"]
+let score = 11 //1st execute do block once then while condt is checked
 
-let arr = 0
-while (arr < myArray.length) {
-    console.log(`Value is ${myArray[arr]}`);
-    arr =  arr + 1
-}
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <10);
